7d48305020b3ea1c21ba97d7b94c2b738259f5db
KseniaLobova <128805342+KseniaLobova@users.noreply.github.com>
2023-09-19 00:37:16 +0200

    little theme

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -12,3 +12,35 @@ function changeActiveColor() {
     headerLinks[1].classList.remove('heder-active');
   }
 }
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const body = document.querySelector('body')
+const checkboxTheme = document.querySelector('.theme-switch__toggle')
+console.log(checkboxTheme)
+checkboxTheme.addEventListener('change', onChengeTheme)
+function onChengeTheme(event) {
+  console.log(event.currentTarget.checked)
+  if (!event.currentTarget.checked) {
+    body.classList.remove('dark-theme')
+    localStorage.setItem('saveTheme', Theme.LIGHT)
+  } else {
+    body.classList.add('dark-theme')
+    localStorage.setItem('saveTheme', Theme.DARK)
+  }
+}
+const saveTheme = localStorage.getItem('saveTheme')
+if (saveTheme === Theme.LIGHT) {
+  body.classList.remove('dark-theme')
+  checkboxTheme.checked = false
+} else {
+  body.classList.add('dark-theme')
+  checkboxTheme.checked = false
+}
+
+
+
+
+
