c12ee2aa4a5953b3fd9160b7ac5aef6f6a4a7af1
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-09-18 10:35:59 +0200

    fixed function onClickHeart+css

--- src/js/recipes/recipes.js
+++ src/js/recipes/recipes.js
@@ -1,79 +1,52 @@
 
-
-// // Цей пошук потрібен лише для відмалювання, коли Діма доробить запит, то ми його видалимо
-
-
-
 import {elements} from '/js/filters/filters.js'
 
-
-// function fetchBreeds() {
-//     const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
-
-//   return fetch(`${BASE_URL}`)
-//     .then((resp) => {
-//     if (!resp.ok) {console.log('Oops! Something went wrong! Try reloading the page!')}
-//     return resp.json();
-//     })
-//     .catch(() =>console.dir('Oops! Something went wrong! Try reloading the page!'))
-// }
-
-//   fetchBreeds().then((data) => {
-    
-//     // const { _id, title, preview, description, rating } = data.results
-//     containerForRecipes.innerHTML = createMarkupElForFilter(data.results)
-    //     }
-//   }).catch(() => {
-//     console.log('err');
-//   })
-
-// важлива create функція
 
 export function createMarkupElForFilter(arr) {
   
  return arr.map(({ _id, title, preview, description, rating }) => 
  
-   `<div class="blok-recipes id="${_id}">
+   `<div class="blok-recipes dark-theme" id="${_id}">
       
-   <input
+      <input
         id="${_id}"
         type="checkbox"
-        class="heart-icon-elem"
+        class="heart-icon-elem dark-theme"
         name="heart-icon"
-        value="off"
+        
       />
-      <label for="heart" aria-hidden="true" class="heart-icon-action">
-        <svg class="icon-heart-svg" width="22" height="22">
+      <label for="${_id}" aria-hidden="true" class="heart-icon-action dark-theme">
+        <svg class="icon-heart-svg dark-theme" width="22" height="22">
           <use href="/img/icon-sprite.svg#icon-heart"></use>
         </svg>
       </label>
 
-       <img class="img-blok-recipes" src="${preview}" alt="${title}" />
+       <img class="img-blok-recipes dark-theme" src="${preview}" alt="${title}" />
 
- <div class="context-blok-recipes"> <h3 class="title-blok-recipes">${title}</h3>
-  <p class="text-blok-recipes">${description}</p>
-  <div class="num-stars-btn"><div class="blok-rating">
-    <p class="text-number-blok-recipes">${rating}</p>
-     <div class="stars">
-     <svg class="star-icon" width="18" height="18">
+ <div class="context-blok-recipes dark-theme"> <h3 class="title-blok-recipes dark-theme">${title}</h3>
+  <p class="text-blok-recipes dark-theme">${description}</p>
+  <div class="num-stars-btn dark-theme"><div class="blok-rating">
+    <p class="text-number-blok-recipes dark-theme">${rating}</p>
+     <div class="stars dark-theme">
+     <svg class="star-icon dark-theme" width="18" height="18">
         <use href="/img/icon-sprite.svg#icon-star"></use>
       </svg>
-      <svg class="star-icon" width="18" height="18">
+      <svg class="star-icon dark-theme" width="18" height="18">
         <use href="/img/icon-sprite.svg#icon-star"></use>
       </svg>
-      <svg class="star-icon" width="18" height="18">
+      <svg class="star-icon dark-theme" width="18" height="18">
         <use href="/img/icon-sprite.svg#icon-star"></use>
       </svg>
-      <svg class="star-icon" width="18" height="18">
+      <svg class="star-icon dark-theme" width="18" height="18">
         <use href="/img/icon-sprite.svg#icon-star"></use>
       </svg>
-      <svg class="star-icon" width="18" height="18">
+      <svg class="star-icon dark-theme" width="18" height="18">
         <use href="/img/icon-sprite.svg#icon-star"></use>
       </svg>
       </div>
       </div>
   
-  <button class="btn-blok-recipes-see" data-modal-open type="button">See recipe</button></div>
+  <button class="btn-blok-recipes-see dark-theme" type="button">See recipe</button></div>
   </div>
 
 </div>`).join(''); 
@@ -81,57 +54,44 @@ export function createMarkupElForFilter(arr) {
 }
 
 export const KEY_FEEDBACK = 'saveCheckedFavorite';
-let arrLocalStor = [];
-export let uniqueArrForLocalStor =JSON.parse(localStorage.getItem(KEY_FEEDBACK)) ?? [];
-let idCard;
 
-// containerForRecipes.innerHTML = createMarkupElForFilter(uniqueArrForLocalStor)
-
-// console.log(iconSvg);
 elements.containerForRecipes.addEventListener('change', onClickHeart)
 
 function onClickHeart(e) {
-  console.log(e.currentTarget);
-  const iconSvg = document.querySelector('.heart-icon-action');
-  if (!e.target) {
-      return
-  }
 
-  if (e.target.value === 'on') {
-    e.target.value = 'off';
-    iconSvg.children[0].classList.remove('svg-active')
+  const uniqueArrForLocalStor = JSON.parse(localStorage.getItem(KEY_FEEDBACK)) ?? [];
+  
+  // const iconSvg = document.querySelector('.icon-heart-svg');
+  // const iconSvg = document.querySelector('.heart-icon-action');
+
+  // console.log(iconSvg);
+
+  const idCard = e.target.id 
+ 
+  if (e.target.checked) {
+    if (!uniqueArrForLocalStor.includes(idCard)) {
+      uniqueArrForLocalStor.push(idCard);
+      // iconSvg.classList.add('svg-active')
+          // iconSvg.children[0].classList.add('svg-active')
+
+      localStorage.setItem(KEY_FEEDBACK, JSON.stringify(uniqueArrForLocalStor));
+    }
+  }
+  
+  if (!e.target.checked) {
+
+    // iconSvg.classList.remove('svg-active')
+  // iconSvg.children[0].classList.remove('svg-active')
+
     
-  console.log("Я видаляю");
-    console.log('idCard', idCard);
-    let indexElCard = uniqueArrForLocalStor.indexOf(idCard);
-    console.log(indexElCard);
+  // console.log("Я видаляю");
+    const indexElCard = uniqueArrForLocalStor.indexOf(idCard);
     uniqueArrForLocalStor.splice(indexElCard, 1);
-    console.log(uniqueArrForLocalStor);
+    // console.log(uniqueArrForLocalStor);
       localStorage.setItem(KEY_FEEDBACK, JSON.stringify(uniqueArrForLocalStor));
 
-  } else {
-    console.log('Я додаю');
-
-  // if (e.target.contains('svg-active')) {
-  //       delete(arrLocalStor[e.target.id]) }
-  
-  //якщо клікнуте
-    if (e.target.checked) {
-    
-      iconSvg.children[0].classList.add('svg-active');
-      e.target.value = 'on';
-      idCard = e.target.id
-      arrLocalStor.push(idCard);
-      console.log(e.target.value);
-      console.log(arrLocalStor);
-    }
-
-
- uniqueArrForLocalStor = arrLocalStor.filter(
-   (elem, index, array) => array.indexOf(elem) === index);
-  console.log(uniqueArrForLocalStor);
-  localStorage.setItem(KEY_FEEDBACK, JSON.stringify(uniqueArrForLocalStor));
- }}
+  } 
+ }
 
 
 //  stars section
