1fc4c4d2a7539f06fccbeb0647f498f3a6254c3a
Oksanamarusich <oksana.m229@gmail.com>
2023-09-18 15:52:07 +0300

    add

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -1,45 +1,37 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
-import axios from 'axios';
-import { createMarkupElForFilter } from '/js/recipes/recipes.js';
+console.log(Pagination);
 
-const options = {
-  totalItems: 0,
-  itemsPerPage: 6,
-  visiblePages: 2,
-  page: 1,
-  centerAlign: false,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-      '</a>'
-  }
+const paginationContainer = document.querySelector(".tui-pagination");
+const options = { // below default value of options
+     totalItems: 0,
+     itemsPerPage: 6,
+     visiblePages: 2,
+     page: 1,
+     centerAlign: false,
+     firstItemClassName: 'tui-first-child',
+     lastItemClassName: 'tui-last-child',
+     template: {
+         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+         moveButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</a>',
+         disabledMoveButton:
+             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</span>',
+         moreButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                 '<span class="tui-ico-ellip">...</span>' +
+             '</a>'
+     }
 };
-
 const pagination = new Pagination('pagination', options);
- 
-pagination.on('afterMove', (event) => {
-    const currentPage = event.page;
-    console.log(`${currentPage}`);
-})
+ console.log(pagination);
 
-
-
-const containerForRecipes = document.querySelector('.container-for-recipes');
-console.log(containerForRecipes);
+let totalItems;
 const page = pagination.getCurrentPage();
 
 async function fetchData() {
@@ -51,37 +43,37 @@ async function fetchData() {
     return recipes;
 }
 
-
-renderfirstPage(page)
-
 function renderfirstPage(page) {
       fetchData(page)
   .then(recipes => {
-      console.log(recipes);
-       totalItems = recipes.totalPages * options.itemsPerPage;
-      console.log(totalItems);
-      pagination.reset(totalItems)
+    console.log(recipes);
+    console.log(recipes.totalPages);
+    console.log(options.itemsPerPage);
+    totalItems = recipes.totalPages * options.itemsPerPage;
+    console.log(totalItems);
+    pagination.reset(totalItems);
 
     })
-          
-    .catch(error => console.log(error));
+          .catch(error => console.log('errorPagination'));
   }
+renderfirstPage(page);
 
 
 function renderEvt(page) {
-      fetchData(page)
+    fetchData(page)
   .then(recipes => {
-      console.log(recipes);
-      pagination.reset(totalItems)
-      
+    console.log(recipes);
+    pagination.reset(totalItems)
+    console.log(totalItems);
   })
         .catch(error => console.log(error));
 }
+
   
- pagination.on('afterMove', (event) => {
+pagination.on('afterMove', (event) => {
   const currentPage = event.page;
   console.log(currentPage);
-  renderEvt(currentPage)
+  renderEvt(currentPage);
 });
 
 
