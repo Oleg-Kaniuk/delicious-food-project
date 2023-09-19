197859e61bf7a3bf5967291524364f70dd569d10
Oksanamarusich <oksana.m229@gmail.com>
2023-09-19 15:26:01 +0300

    pagination works

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -4,84 +4,82 @@ import 'tui-pagination/dist/tui-pagination.css';
 import { createMarkupElForFilter } from '/js/recipes/recipes.js';
 
 const containerForRecipes = document.querySelector(".container-for-recipes");
-const paginationContainer = document.querySelector(".tui-pagination");
+const paginationContainer = document.querySelector("#pagination");
 
 const options = { 
      totalItems: 0,
      itemsPerPage: 6,
-     visiblePages: 3,
+     visiblePages: 2,
      page: 1,
-     centerAlign: true,
-     firstItemClassName: 'tui-first-child',
-     lastItemClassName: 'tui-last-child',
-     template: {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
+  centerAlign: true,
+  //функція що відслідковує кліки та змінює поточну сторінку та викликає fetchData() для нової сторінки
+  onPageClick: function (event) {
+    const clickedPage = event.page;
+    if (pagePagination !== clickedPage) {
+      pagePagination = clickedPage;
+      fetchData(pagePagination);
+    }
+      },
 };
-const pagination = new Pagination('pagination', options);
- console.log(pagination);
 
+const pagination = new Pagination(paginationContainer, options);
 let totalItems;
 const page = pagination.getCurrentPage();//почaткатова сторінка з опцій
-console.log(page);
+let pagePagination = 1; // початкова сторінка
+const itemsPerPage = 6; //на сторінці
 
-async function fetchData() {
+async function fetchData(pagePagination) {
     const BASEURL_RECIPES =
     'https://tasty-treats-backend.p.goit.global/api/recipes';
-    const response = await fetch(`${BASEURL_RECIPES}`);
+    const response = await fetch(`${BASEURL_RECIPES}?page=${pagePagination}&limit=${itemsPerPage}`);
     const recipes = await response.json();
-    console.log(recipes);
     return recipes;
 }
 // генерує першу сторінку з інформацією  про к-сть всіх елементів 
 function renderfirstPage(page) {
       fetchData(page)
-  .then(recipes => {
-    console.log(recipes);
-    console.log(recipes.totalPages);
-    console.log(options.itemsPerPage);
-    totalItems = recipes.totalPages * options.itemsPerPage;
-    console.log(totalItems);
-    pagination.reset(totalItems);//передає заг к-сть елементів з бекенду в пагінацію
+        .then(recipes => {
+          pagePagination++;
+          totalItems = recipes.totalPages * options.itemsPerPage;
+          pagination.reset(totalItems);//передає заг к-сть елементів з бекенду в пагінацію
    
     })
-          .catch(error => console.log('errorPagination'));
+        .catch(error => console.log('errorPagination'));
   }
 renderfirstPage(page);
-console.log(page);
+
 
 //генерує наступну сторінку 
 function renderEvt(page) {
-  console.log(page);
   fetchData(page)
   .then(recipes => {
     console.log(recipes);
+    pagePagination++;
     containerForRecipes.innerHTML = createMarkupElForFilter(recipes.results);
-    console.log(recipes.results);
+  
   })
         .catch(error => console.log(error));
 }
 
-  // тут мають рендеритись різні сторінки за допомогою цього методу
+  //  рендерznmcz різні сторінки за допомогою цього методу
 pagination.on('afterMove', (event) => {
   const currentPage = event.page;
   console.log(currentPage);// це поточна сторінка яка має передаватись у ф-ю генерації інших сторінок
+  if (currentPage < pagePagination) {
+    renderEvt(currentPage);
+  }
   renderEvt(currentPage);
 });
 
 
+const getCardPerPage = () => {
+    const windowWidth = document.documentElement.clientWidth;
 
-
+    if (windowWidth < 768) {
+        return '6';
+    } else if (windowWidth < 1280) {
+        return '8';
+    } else {
+        return '9';
+    }
+};
