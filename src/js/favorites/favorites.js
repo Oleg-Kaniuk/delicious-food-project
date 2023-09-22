32b8a8557df0012e9d23252a67871f302b752b02
IrinaaMi <iro4ka.m.n@gmail.com>
2023-09-22 07:10:52 +0200

    add styles

--- src/js/favorites/favorites.js
+++ src/js/favorites/favorites.js
@@ -5,8 +5,9 @@ import { getRecipesByCategory } from '/js/categories/categories.js';
 import { onModal, backdropElem } from '/js/modal-recipe/modal-recipe.js';
 const favList = document.querySelector('.favorite-recipes-list');
 const categories = document.querySelector('.categories');
-const emptyFav = document.querySelector('empty-favorites-container');
 const favorite = JSON.parse(localStorage.getItem('saveCheckedFavorite')) || [];
+const allBtn = document.querySelector('.category-btn')
+const categoryButtons = document.querySelectorAll('.category-button')
 const uniqueCategories = new Set();
 
 const ul = document.querySelector;
@@ -14,27 +15,28 @@ const favoritesContainer = document.querySelector('.empty-favorites-container');
 const BASE_FAV_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes';
 let currentCategory = null;
 let cardArr = '';
+
 async function fetchFavImages(id) {
-  try {
-    const response = await axios.get(`${BASE_FAV_URL}/${id}`);
-    return response;
-  } catch (error) {
-    console.error(`Failed to fetch images: ${error}`);
-  }
+    try {
+        const response = await axios.get(`${BASE_FAV_URL}/${id}`);
+        return response;
+    } catch (error) {
+        console.error(`Failed to fetch images: ${error}`);
+    }
 }
 
 function createMarkupForFilter(el) {
-  const { _id, title, preview, description, rating, category } = el;
+    const { _id, title, preview, description, rating, category } = el;
 
-  const idFromLocalSorage =
-    JSON.parse(localStorage.getItem('saveCheckedFavorite')) || [];
-  const includesIdAtLocalStorage = idFromLocalSorage.includes(el);
-  const heartClass = includesIdAtLocalStorage
-    ? 'heart-icon-action  svg-active'
-    : 'heart-icon-action ';
-  const checked = includesIdAtLocalStorage;
+    const idFromLocalSorage =
+        JSON.parse(localStorage.getItem('saveCheckedFavorite')) || [];
+    const includesIdAtLocalStorage = idFromLocalSorage.includes(el);
+    const heartClass = includesIdAtLocalStorage ?
+        'heart-icon-action  svg-active' :
+        'heart-icon-action ';
+    const checked = includesIdAtLocalStorage;
 
-  return `<li class="blok-recipes blok-recipes-fav" id="${_id}">
+    return `<li class="blok-recipes blok-recipes-fav" id="${_id}">
         
         <input
           id="${_id}"
@@ -83,42 +85,42 @@ function createMarkupForFilter(el) {
 }
 
 if (favorite.length > 0) {
-  favorite.map(idEl => {
-    fetchFavImages(idEl).then(data => {
-      favList.insertAdjacentHTML('beforeend', createMarkupForFilter(data.data));
-      createGoldStarOneEl(data.data);
-      const btnSeeRecipe = document.querySelectorAll('.btn-blok-recipes-see');
-      [...btnSeeRecipe].forEach(function (card) {
-        const id = card.id;
-        card.addEventListener('click', () => {
-          onBtnRecipeClick(id);
-        });
-      });
+    favorite.map(idEl => {
+        fetchFavImages(idEl).then(data => {
+            favList.insertAdjacentHTML('beforeend', createMarkupForFilter(data.data));
+            createGoldStarOneEl(data.data);
+            const btnSeeRecipe = document.querySelectorAll('.btn-blok-recipes-see');
+            [...btnSeeRecipe].forEach(function(card) {
+                const id = card.id;
+                card.addEventListener('click', () => {
+                    onBtnRecipeClick(id);
+                });
+            });
 
-      const recipeCategory = data.data.category;
-      if (recipeCategory) {
-        uniqueCategories.add(recipeCategory);
-        const categoryButtonsHTML = [...uniqueCategories]
-          .map(
-            category => `
+            const recipeCategory = data.data.category;
+            if (recipeCategory) {
+                uniqueCategories.add(recipeCategory);
+                const categoryButtonsHTML = [...uniqueCategories]
+                    .map(
+                        category => `
       <button class="category-button" data-category="${category}">${category}</button>
   `
-          )
-          .join('');
+                    )
+                    .join('');
 
-        categories.innerHTML = categoryButtonsHTML;
-      }
+                categories.innerHTML = categoryButtonsHTML;
+            }
+        });
     });
-  });
 } else {
-  favList.innerHTML = '';
-  categories.innerHTML = '';
-
-  favoritesContainer.innerHTML = createMarkupForEmptyFav();
+    favList.innerHTML = '';
+    categories.innerHTML = '';
+    allBtn.style.display = 'none';
+    favoritesContainer.innerHTML = createMarkupForEmptyFav();
 }
 
 function createMarkupForEmptyFav() {
-  return `
+    return `
 <svg width="68" height="58" viewBox="0 0 37 32">
 <use xlink:href="../../img/icon-sprite.svg#icon-hat"></use>
 </svg>
@@ -128,87 +130,76 @@ function createMarkupForEmptyFav() {
 }
 
 if (favList) {
-  favList.addEventListener('change', clickHeart);
+    favList.addEventListener('change', clickHeart);
 }
 
 function clickHeart(e) {
-  const idCard = e.target.id;
-  const indexElCard = favorite.indexOf(idCard);
+    const idCard = e.target.id;
+    const indexElCard = favorite.indexOf(idCard);
 
-  if (indexElCard !== -1) {
-    favorite.splice(indexElCard, 1);
+    if (indexElCard !== -1) {
+        favorite.splice(indexElCard, 1);
 
-    localStorage.setItem('saveCheckedFavorite', JSON.stringify(favorite));
+        localStorage.setItem('saveCheckedFavorite', JSON.stringify(favorite));
 
-    const cardElement = document.getElementById(`${idCard}`);
-    const removedCategory = cardElement.getAttribute('data-category');
-    cardElement.remove();
-    location.reload();
-    const categoryButtons = document.querySelectorAll('.category-buttons');
-    cardArr = categoryButtons;
+        const cardElement = document.getElementById(`${idCard}`);
+        const removedCategory = cardElement.getAttribute('data-category');
+        cardElement.remove();
+        location.reload();
+        const categoryButtons = document.querySelectorAll('.category-buttons');
+        cardArr = categoryButtons;
 
-    updateCategories(removedCategory);
-    if (favorite.length === 0) {
-      const emptyFavoriteMarkup = createMarkupForEmptyFav();
-      const favoritesContainer = document.getElementById('favoritesContainer');
+        updateCategories(removedCategory);
+        if (favorite.length === 0) {
+            const emptyFavoriteMarkup = createMarkupForEmptyFav();
+            const favoritesContainer = document.getElementById('favoritesContainer');
 
-      if (favoritesContainer) {
-        favoritesContainer.innerHTML = emptyFavoriteMarkup;
-      }
+            if (favoritesContainer) {
+                favoritesContainer.innerHTML = emptyFavoriteMarkup;
+            }
+        }
     }
-  }
 }
 
 // видалення категорій. працює тільки коли обновити сторінку
 function updateCategories(removedCategory) {
-  const existingCategories = new Set();
+    const existingCategories = new Set();
 
-  favList.querySelectorAll('.blok-recipes').forEach(recipe => {
-    const category = recipe.getAttribute('data-category');
-    if (category) {
-      existingCategories.add(category);
-    }
-  });
-
-  if (removedCategory) {
-    categoryButtons.forEach(categoryButton => {
-      console.dir(categoryButton.dataset);
-      const category = categoryButton.getAttribute('data-category');
-      if (category === removedCategory) {
-        categoryButton.remove();
-        existingCategories.delete(category);
-        console.dir(category);
-
-        console.dir(existingCategories);
-      }
+    favList.querySelectorAll('.blok-recipes').forEach(recipe => {
+        const category = recipe.getAttribute('data-category');
+        if (category) {
+            existingCategories.add(category);
+        }
     });
-  }
+
+    if (removedCategory) {
+        categoryButtons.forEach(categoryButton => {
+            const category = categoryButton.getAttribute('data-category');
+            if (category === removedCategory) {
+                categoryButton.remove();
+                existingCategories.delete(category);
+            }
+        });
+    }
 }
 
 export function createGoldStarOneEl() {
-  const cards = document.querySelectorAll('.blok-recipes-fav');
-  cards.forEach(card => {
-    const rating = parseFloat(
-      card.querySelector('.text-number-blok-recipes').textContent
-    );
-    const stars = card.querySelectorAll('.star-icon');
+    const cards = document.querySelectorAll('.blok-recipes-fav');
+    cards.forEach(card => {
+        const rating = parseFloat(
+            card.querySelector('.text-number-blok-recipes').textContent
+        );
+        const stars = card.querySelectorAll('.star-icon');
 
-    for (let i = 0; i < stars.length; i++) {
-      if (i < Math.floor(rating)) {
-        stars[i].classList.add('star-color-icon');
-      }
-    }
-  });
+        for (let i = 0; i < stars.length; i++) {
+            if (i < Math.floor(rating)) {
+                stars[i].classList.add('star-color-icon');
+            }
+        }
+    });
 }
 
-// ф-я для категорій
-// function createBtnCateg(arr) {
-//   return  arr.map(category => `<button class="category-button" data-category="${category}">${category}</button>
-//   `).join('');
-
-// }
-
 function onBtnRecipeClick(id) {
-  backdropElem.classList.remove('is-hidden-recipe-backdrop');
-  onModal(id);
+    backdropElem.classList.remove('is-hidden-recipe-backdrop');
+    onModal(id);
 }
